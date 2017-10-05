20c7c479f9d1c990f925a5fec89b17e1b881c2e9
ingevers4 <ingevers4@student.arteveldehs.be>
2017-10-05 10:08:36 +0200

    WIP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$x = 10;
+$y = 7;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "7 + 10 is ", $y + $x ?> <br>
+    <?php echo "7 - 10 is ", $y - $x ?> <br>
+    <?php echo "7 * 10 is ", $y * $x ?> <br>
+    <?php echo "7 / 10 is ", $y / $x ?> <br>
+    <?php echo "7 % 10 is ", $x % $y ?> <br>
+
+
+</body>
+</html>
